6be378d12446091359b99171b2b880e4553f8331
Nicolas nackphouminh <n.nackphouminh@gmail.com>
2024-01-09 12:21:57 +0100

    Demo 7 - Entity Framework with SQL Server : save after DB init in console

---
+++ Module7DemoEntity/AppContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7DemoEntity
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Personne> Personnes {  get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;" + 
+                "Database=Module7DemoEntity;" + 
+                "MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ Module7DemoEntity/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module7DemoEntity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
