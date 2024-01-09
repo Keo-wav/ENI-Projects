4fc2a2f1196a78ffa890fcb998ecc8ec3742d0c3
Nicolas nackphouminh <n.nackphouminh@gmail.com>
2024-01-09 17:02:48 +0100

    TP Dojo v2 - setup save

---
+++ TPDojo/BO/Samurai.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    internal class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public override Weapon? Weapon { get; set; }
+    }
+}
---
+++ TPDojo/BO/Weapon.cs
@@ -0,0 +1,9 @@
+﻿namespace BO
+{
+    public class Weapon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Damage { get; set; }
+    }
+}
