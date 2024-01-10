b488c083dcee2ced86a5d6839cf1dc39ee946659
Nicolas nackphouminh <n.nackphouminh@gmail.com>
2024-01-10 09:38:06 +0100

    TP Dojo redux : niveau 1 done (created Weapons controller & Index list in Navbar, created 4 weapons)

---
+++ TPDojo/TPDojo1/Data/TPDojoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BO;
+
+    public class TPDojoContext : DbContext
+    {
+        public TPDojoContext (DbContextOptions<TPDojoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BO.Weapon> Weapon { get; set; } = default!;
+    }
--- TPDojo/TPDojo1/Program.cs
+++ TPDojo/TPDojo1/Program.cs
@@ -1,3 +1,5 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 namespace TPDojo1
 {
     public class Program
@@ -5,6 +7,8 @@ namespace TPDojo1
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<TPDojoContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("TPDojoContext") ?? throw new InvalidOperationException("Connection string 'TPDojoContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
