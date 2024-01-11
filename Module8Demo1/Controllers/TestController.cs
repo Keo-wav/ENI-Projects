4ff276b134fb89a5de3ac3b8ad63340d3502fdf5
Nicolas nackphouminh <n.nackphouminh@gmail.com>
2024-01-11 11:19:43 +0100

    Module 8 demo : part 2 (added an index view for registered users

---
+++ Module8Demo1/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Module8Demo1.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public TestController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            List<IdentityUser> users = userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
